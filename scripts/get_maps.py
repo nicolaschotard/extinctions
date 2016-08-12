8349897fecb4c3fadc2a93598d340c90ca3e6520
Nicoals Chotard <nchotard@in2p3.fr>
2016-08-12 14:21:02 +0200

    Test new options and add info in the maps file

--- scripts/get_maps.py
+++ scripts/get_maps.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import yaml
 import wget
 from argparse import ArgumentParser
@@ -25,10 +26,12 @@ if __name__ == "__main__":
     parser.add_argument("--outdir",
                         help="Output directory in where to put the dust maps")
     parser.add_argument("--update", action='store_true', default=False,
-                        help="Update the maps.yaml file")
-    parser.add_argument("--distant", action='store_true', default=False,
-                        help="Copy the maps list from the distant github"
-                        " repository instead of using the local one")
+                        help="Update the maps directory in case of changes of maps.yaml")
+    parser.add_argument("--list", action='store_true', default=False,
+                        help="List of available maps and exit")
+    parser.add_argument("--select", help="Select maps to download (coma separated)")
+    parser.add_argument("--exclude", help="Exclude map(s) (coma separated)."
+                        "If the select option is used, the exclude option will be ignored.")
     args = parser.parse_args()
 
     if args.outdir is not None:
@@ -46,18 +49,43 @@ if __name__ == "__main__":
     else:
         print "INFO: output directory used is", outdir
 
-    if args.distant:
-        m = "https://raw.githubusercontent.com/nicolaschotard/Extinction/master/maps/maps.yaml"
-        print "INFO: downloading the map list from the distant github repository: %s\n" % m
-        download(m, outdir, update=args.update)
-    else:
-        m = resource_filename('Extinction', 'data/maps.yaml')
-        print "INFO: getting the map list from the local github repository: %s\n" % m
-        copy2(m, outdir)
+    # Get the maps yaml file
+    m = resource_filename('Extinction', 'data/maps.yaml')
+    print "INFO: getting the map list from the local github repository: %s" % m
+    copy2(m, outdir)
+
+    # Now get all or part of the maps
     maps = yaml.load(open(outdir+"/maps.yaml"))
-    print "\nINFO: Downloading the following maps:"
+
+    # List the maps
+    if args.list:
+        print "INFO: Available maps are"
+        for i, m in enumerate(sorted(maps)):
+            print "       %i: %s (%s) - %s" % (i, m, maps[m]['size'], maps[m]['url'])
+        sys.exit()
+        
+    # Selection by the user?
+    if args.select is not None:
+        select = args.select.split(',')
+        for m in maps.keys():
+            if m not in select:
+                maps.pop(m)
+    elif args.exclude is not None:
+        exclude = args.exclude.split(',')
+        for m in maps.keys():
+            if m in exclude:
+                maps.pop(m)
+
+    # Check if some maps are already present
+    for m in maps.keys():
+        path = outdir+"/"+os.path.basename(maps[m]['url'])
+        if os.path.exists(path):
+            print " File", path, "already exist."
+            maps.pop(m)
+    
+    print "INFO: Downloading the following maps:"
     for i, m in enumerate(sorted(maps)):
         print "       %i: %s" % (i, m)
     print
     for m in sorted(maps):
-        download(maps[m], outdir)
+        download(maps[m]['url'], outdir)
