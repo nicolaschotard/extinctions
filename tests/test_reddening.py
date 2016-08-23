4ef16563d4687d281065fcbb08d5d680167e21b5
Nicoals Chotard <nchotard@in2p3.fr>
2016-08-23 15:27:54 +0200

    coverage should now gives the right output

--- tests/test_reddening.py
+++ tests/test_reddening.py
@@ -1,78 +1,62 @@
 """Test the reddening module."""
 
-import pytest
 from Extinction import reddening
 
 
+# Test values
 RA = 340.83
 DEC = -9.59
 
 
-@pytest.fixture()
-def float_coordinates():
-    """Return RA DEC as two floats."""
-    return RA, DEC
+# Test the Reddening class
+
+def test_init_float():
+    """Initialize with two floats."""
+    reddening.Reddening(RA, DEC)
 
 
-@pytest.fixture()
-def list_coordinates():
-    """Return RA DEC as two lists."""
-    return [RA], [DEC]
+def test_init_list():
+    """Initialize with two list."""
+    reddening.Reddening([RA], [DEC])
 
 
-@pytest.fixture()
-def mixt_coordinates():
-    """Return RA as a float, DEC as a list."""
-    return RA, [DEC]
+def test_init_mixt():
+    """Initialize with one float and one list."""
+    reddening.Reddening(RA, [DEC])
 
 
-@pytest.fixture()
-def get_reddening(float_coordinates):
-    """Get the Reddening object."""
-    ra, dec = float_coordinates
-    return reddening.Reddening(ra, dec)
+def test_astroquery():
+    """Test the astroquery wrapper."""
+    red = reddening.Reddening(RA, DEC)
+    red.from_astroquery(dustmap='SFD98')
+    red.from_astroquery(dustmap='SF11')
 
 
-class TestReddening(object):
+def test_snfactory():
+    """Test the 'snfactory' query method."""
+    red = reddening.Reddening(RA, DEC)
+    red.from_snfactory()
 
-    """Test the Reddening class."""
 
-    def test_init_float(self, float_coordinates):
-        """Initialize with two floats."""
-        ra, dec = float_coordinates
-        reddening.Reddening(ra, dec)
+def test_argonaut():
+    """Test the query to the argonaut server."""
+    red = reddening.Reddening(RA, DEC)
+    red.from_argonaut()
 
-    def test_init_list(self, list_coordinates):
-        """Initialize with two list."""
-        ra, dec = list_coordinates
-        reddening.Reddening(ra, dec)
 
-    def test_init_mixt(self, mixt_coordinates):
-        """Initialize with one float and one list."""
-        ra, dec = mixt_coordinates
-        reddening.Reddening(ra, dec)
+def test_sncosmo():
+    """Test the sncosmo wrapper."""
+    red = reddening.Reddening(RA, DEC)
+    red.from_sncosmo()
 
-    def test_astroquery(self, get_reddening):
-        """Test the astroquery wrapper."""
-        get_reddening.from_astroquery(dustmap='SFD98')
-        get_reddening.from_astroquery(dustmap='SF11')
 
-    def test_snfactory(self, get_reddening):
-        """Test the 'snfactory' query method."""
-        get_reddening.from_snfactory()
+def test_sfd_map():
+    """Test the local query on the SFD map."""
+    red = reddening.Reddening(RA, DEC)
+    red.query_local_map(dustmap='sfd')
 
-    def test_argonaut(self, get_reddening):
-        """Test the query to the argonaut server."""
-        get_reddening.from_argonaut()
 
-    def test_sncosmo(self, get_reddening):
-        """Test the sncosmo wrapper."""
-        get_reddening.from_sncosmo()
-
-    def test_sfd_map(self, get_reddening):
-        """Test the local query on the SFD map."""
-        get_reddening.query_local_map(dustmap='sfd')
-
-    def test_schlafly_map(self, get_reddening):
-        """Test the local query on the Schlafly map."""
-        get_reddening.query_local_map(dustmap='schlafly')
+def test_schlafly_map():
+    """Test the local query on the Schlafly map."""
+    red = reddening.Reddening(RA, DEC)
+    red.query_local_map(dustmap='schlafly')
