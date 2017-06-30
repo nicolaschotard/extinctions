89022f17014d0fc25007540e5532b2fb643f75ed
Nicoals Chotard <nchotard@in2p3.fr>
2017-06-30 15:32:43 +0200

    Use Agg backend if no display

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -1,5 +1,9 @@
 """Test the scripts."""
 
+import matplotlib as mpl
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using non-interactive Agg backend')
+    mpl.use('Agg')
 from extinctions import main
 
 
